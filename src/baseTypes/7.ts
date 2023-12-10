bd48ac4c03fbd6906d2a79c4fa41c58c34d9cd6e
Yevhenii Datsenko <yevheniydatsenko@gmail.com>
2023-12-10 12:01:02 +0200

    Final commit

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,19 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum DaysOfWeek {
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+  SUNDAY,
+}
+
+function isWeekend(day: DaysOfWeek): boolean {
+  const weekendDays: DaysOfWeek[] = [DaysOfWeek.SATURDAY, DaysOfWeek.SUNDAY];
+
+  return weekendDays.includes(day);
+}
--- src/generic/6.ts
+++ src/generic/6.ts
@@ -12,12 +12,10 @@ type User = {
   surname: string;
   email: string;
   password: string;
-}
+};
 
-function createOrUpdateUser(initialValues: User) {
-  // Оновлення користувача
-}
+function createOrUpdateUser(initialValues: Partial<User>) {}
 
-createOrUpdateUser({ email: 'user@mail.com', password: 'password123' });
+createOrUpdateUser({ email: "user@mail.com", password: "password123" });
 
 export {};
